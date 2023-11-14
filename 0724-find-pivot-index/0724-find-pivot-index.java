d2129fcffd297c19a18257eda5df1d7bc0e4b84e
Puneeth C <puneethchoppanati@gmail.com>
2023-11-14 12:58:26 +0530

    Time: 1 ms (99.08%), Space: 44.2 MB (39.76%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+        }
+        int left = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(sum - nums[i] - left == left){
+                return i;
+            }
+            left += nums[i];
+        }
+        return -1;
+    }
+}
